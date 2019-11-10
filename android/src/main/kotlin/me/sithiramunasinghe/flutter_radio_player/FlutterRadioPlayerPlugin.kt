1c2014bfdc2528b36d70fa3265ec54745a111b30
Sithira Munasinghe <sithiraac@gmail.com>
2019-11-10 11:59:08 +0530

    Initial Commit

---
+++ android/src/main/kotlin/me/sithiramunasinghe/flutter_radio_player/FlutterRadioPlayerPlugin.kt
@@ -0,0 +1,25 @@
+package me.sithiramunasinghe.flutter_radio_player
+
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+class FlutterRadioPlayerPlugin: MethodCallHandler {
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val channel = MethodChannel(registrar.messenger(), "flutter_radio_player")
+      channel.setMethodCallHandler(FlutterRadioPlayerPlugin())
+    }
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+}
