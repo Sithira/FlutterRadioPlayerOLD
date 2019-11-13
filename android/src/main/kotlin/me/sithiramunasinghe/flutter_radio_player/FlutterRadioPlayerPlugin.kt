99025f5535bb9f1c6e0606fb61c81b78a59a390c
Sithira Munasinghe <sithiraac@gmail.com>
2019-11-13 08:24:32 +0530

    Added more simplified version of ExoPlayer implementation.

--- android/src/main/kotlin/me/sithiramunasinghe/flutter_radio_player/FlutterRadioPlayerPlugin.kt
+++ android/src/main/kotlin/me/sithiramunasinghe/flutter_radio_player/FlutterRadioPlayerPlugin.kt
@@ -1,27 +1,21 @@
 package me.sithiramunasinghe.flutter_radio_player
 
 import android.content.ComponentName
-import android.content.Intent
 import android.content.ServiceConnection
 import android.os.IBinder
-import com.google.android.exoplayer2.ExoPlayer
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
-import me.sithiramunasinghe.flutter_radio_player.player.RadioPlayerService
 
 class FlutterRadioPlayerPlugin: MethodCallHandler {
-
-  private lateinit var player: ExoPlayer
 
   companion object {
     @JvmStatic
     fun registerWith(registrar: Registrar) {
       val channel = MethodChannel(registrar.messenger(), "flutter_radio_player")
       channel.setMethodCallHandler(FlutterRadioPlayerPlugin())
-      var intentService = Intent(registrar.context(), RadioPlayerService::class.java)
     }
   }
 
@@ -33,18 +27,14 @@ class FlutterRadioPlayerPlugin: MethodCallHandler {
     }
   }
 
-   var serviceConnection : ServiceConnection = object : ServiceConnection {
-     override fun onServiceDisconnected(p0: ComponentName?) {
-       TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-     }
+  val serviceConnection = object : ServiceConnection {
+    override fun onServiceDisconnected(name: ComponentName?) {
+      TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
-     override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-       if (service is RadioPlayerService.PlayerServiceBinder) {
-         // Get the instance of the player from the service and set it as player to our playerView
-         player = service.getPlayerHolderInstance().audioFocusPlayer
-         player
-       }
-     }
+    override fun onServiceConnected(name: ComponentName?, binder: IBinder?) {
+      TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
-   }
+  }
 }
--- android/src/main/kotlin/me/sithiramunasinghe/flutter_radio_player/player/RadioPlayerService.kt
+++ android/src/main/kotlin/me/sithiramunasinghe/flutter_radio_player/player/RadioPlayerService.kt
@@ -1,63 +1,212 @@
 package me.sithiramunasinghe.flutter_radio_player.player
 
-import android.app.IntentService
 import android.app.Notification
+import android.app.PendingIntent
+import android.app.Service
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.media.session.MediaSession
+import android.net.Uri
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
+import android.support.annotation.Nullable
+import android.support.v4.media.session.MediaSessionCompat
+import com.google.android.exoplayer2.C
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.ui.PlayerNotificationManager
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
+import com.google.android.exoplayer2.util.Util.getUserAgent
+import me.sithiramunasinghe.flutter_radio_player.R
 
-class RadioPlayerService : IntentService("flutter_radio_plugin"), PlayerNotificationManager.NotificationListener {
 
-    companion object {
-        const val NOTIFICATION_ID = 100
-        const val NOTIFICATION_CHANNEL = "flutter_radio_plugin_channel"
+class RadioPlayerService : Service() {
+
+    private var isBound = false
+
+    private val iBinder = LocalBinder()
+
+    // context
+    private val context = this
+
+    // class instances
+    private var player: SimpleExoPlayer? = null
+    private var mediaSessionConnector: MediaSessionConnector? = null
+    private var mediaSession: MediaSession? = null
+    private val playerNotificationManager: PlayerNotificationManager? = null
+
+    // session keys
+    private val playbackChannelId = "flutter_radio_player_channel_id"
+    private val playbackNotificationId = 1025
+    private val mediaSessionId = "flutter_radio_radio_media_session"
+
+    // stream URL
+    private var streamURL: String? = null
+
+    inner class LocalBinder : Binder() {
+        internal val service: RadioPlayerService
+            get() = this@RadioPlayerService
     }
 
-    private lateinit var playerHolder: PlayerHolder
+    override fun onDestroy() {
+        super.onDestroy()
 
-    private lateinit var playerNotificationManager: PlayerNotificationManager
+        streamURL = ""
 
-
-    inner class PlayerServiceBinder : Binder() {
-        fun getPlayerHolderInstance() = playerHolder
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-
-        // Build a player holder
-        playerHolder = PlayerModule.getPlayerHolder(this)
-
-        /** Build a notification manager for our player, set a notification listener to this,
-         * and assign the player just created.
-         *
-         * It is very important to note we need to get a [PlayerNotificationManager] instance
-         * via the [PlayerNotificationManager.createWithNotificationChannel] because targeting Android O+
-         * when building a notification we need to create a channel to which assign it.
-         */
-
-
-        playerNotificationManager = PlayerModule.getPlayerNotificationManager(this, this)
-                .also {
-                    it.setPlayer(playerHolder.audioFocusPlayer)
-                }
-    }
-
-    override fun onBind(intent: Intent?): IBinder {
-        intent?.let {
-            playerHolder.start()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mediaSession?.release()
         }
-        return PlayerServiceBinder()
+
+        mediaSessionConnector?.setPlayer(null)
+        playerNotificationManager?.setPlayer(null)
+        player?.release()
+        player = null
     }
 
-    override fun onHandleIntent(p0: Intent?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBind(intent: Intent?): IBinder? {
+        return iBinder
     }
 
-    override fun onNotificationCancelled(notificationId: Int) {}
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-    override fun onNotificationStarted(notificationId: Int, notification: Notification?) {
-        startForeground(notificationId, notification)
+        // get details
+        val title = intent!!.getStringExtra("title")
+        val channel = intent.getStringExtra("channel")
+        val url = intent.getStringExtra("url")
+
+        streamURL = url
+
+        player = ExoPlayerFactory.newSimpleInstance(this, DefaultTrackSelector())
+
+        val dataSourceFactory = DefaultDataSourceFactory(context, getUserAgent(context, "flutter_radio_player"))
+
+        val audioSource = buildMediaSource(dataSourceFactory, streamURL!!)
+
+        val playerEvents = object : Player.EventListener {
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                if (playbackState == SimpleExoPlayer.STATE_IDLE) {
+                    //methodChannel.invokeMethod("onPlayerStateChanged", "idle")
+                } else if (playbackState == SimpleExoPlayer.STATE_BUFFERING) {
+                    //methodChannel.invokeMethod("onPlayerStateChanged", "buffering")
+                    // add buffering string
+                } else if (playbackState == SimpleExoPlayer.STATE_ENDED) {
+                    //methodChannel.invokeMethod("onPlayerCompleted", null)
+                    //methodChannel.invokeMethod("onPlayerStateChanged", "idle")
+                    stopSelf()
+                } else if (playWhenReady) {
+                    //methodChannel.invokeMethod("onPlayerStateChanged", "playing")
+                    // add playing
+                } else {
+                    // methodChannel.invokeMethod("onPlayerStateChanged", "paused")
+                    // add paused
+                }
+            }
+        }
+
+        player?.let {
+            it.addListener(playerEvents)
+            it.playWhenReady = true
+            it.prepare(audioSource)
+        }
+
+        val playerNotificationManager = PlayerNotificationManager.createWithNotificationChannel(
+                context,
+                playbackChannelId,
+                R.string.channel_name,
+                R.string.channel_description,
+                playbackNotificationId,
+                object : PlayerNotificationManager.MediaDescriptionAdapter {
+                    override fun getCurrentContentTitle(player: Player): String? {
+                        return title
+                    }
+
+                    @Nullable
+                    override fun createCurrentContentIntent(player: Player): PendingIntent? {
+                        /*
+                        Intent intent = new Intent(context, MainActivity.class);
+                        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                        */
+                        return null
+                    }
+
+                    @Nullable
+                    override fun getCurrentContentText(player: Player): String? {
+                        return channel
+                    }
+
+                    @Nullable
+                    override fun getCurrentLargeIcon(player: Player, callback: PlayerNotificationManager.BitmapCallback): Bitmap? {
+                        return if (intent.getStringExtra("bigIcon") == null) {
+                            null
+                        } else {
+
+                            // todo: plugin reg
+//                            val resourceId = pluginRegistrar.context().getResources().getIdentifier(
+//                                    intent.getStringExtra("bigIcon"),
+//                                    "drawable",
+//                                    pluginRegistrar.context().getPackageName())
+//                            BitmapFactory.decodeResource(
+//                                    pluginRegistrar.context().getResources(),
+//                                    resourceId
+//                            )
+
+                            return null
+
+                        }
+                    }
+                },
+                object : PlayerNotificationManager.NotificationListener {
+                    override fun onNotificationCancelled(notificationId: Int, dismissedByUser: Boolean) {
+                        isBound = false
+                        // audioService = null
+                        stopSelf()
+                    }
+
+                    override fun onNotificationPosted(notificationId: Int, notification: Notification, ongoing: Boolean) {
+                        startForeground(notificationId, notification)
+                    }
+                }
+        )
+
+
+        val mediaSession = MediaSessionCompat(context, mediaSessionId)
+        mediaSession.isActive = true
+
+
+        mediaSessionConnector = MediaSessionConnector(mediaSession)
+        mediaSessionConnector?.setPlayer(player)
+
+        playerNotificationManager.setUsePlayPauseActions(true)
+        playerNotificationManager.setUseNavigationActions(false)
+        playerNotificationManager.setPlayer(player)
+        playerNotificationManager.setMediaSessionToken(mediaSession.sessionToken)
+
+        return START_STICKY
     }
+
+    /**
+     *
+     */
+    private fun buildMediaSource(dataSourceFactory: DefaultDataSourceFactory, streamUrl: String): MediaSource {
+
+        val uri = Uri.parse(streamUrl)
+
+        return when (val type = Util.inferContentType(uri)) {
+            C.TYPE_HLS -> HlsMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
+            C.TYPE_OTHER -> ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
+            else -> {
+                throw IllegalStateException("Unsupported type: $type")
+            }
+        }
+    }
+
 }
