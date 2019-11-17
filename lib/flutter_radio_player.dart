184b40f98d11b8e4349b4f45910c10594629b31b
Sithira Munasinghe <sithiraac@gmail.com>
2019-11-17 06:16:25 +0530

    Added DocBlocks

--- lib/flutter_radio_player.dart
+++ lib/flutter_radio_player.dart
@@ -1,17 +1,17 @@
 import 'dart:async';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 class FlutterRadioPlayer {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_radio_player');
+
+  static const MethodChannel _channel = const MethodChannel('flutter_radio_player');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
+  /// Initializes Services, Binds Audio and Starts audio playback.
   static Future<void> init(String title, String channel, String url,
       String albumCover, String appIcon) async {
     String checkIfNull(String toCheck) {
@@ -21,8 +21,6 @@ class FlutterRadioPlayer {
         return toCheck;
       }
     }
-
-    debugPrint("init invoked... calling native methods");
 
     await checkIfBound();
 
@@ -35,28 +33,31 @@ class FlutterRadioPlayer {
     });
   }
 
+  /// Stops and unbinds audio instances.
   static Future<void> stop() async {
     await checkIfBound();
     await _channel.invokeMethod("stop");
   }
 
-  /// Will pause the player. If player already paused will do nothing
+  /// Pause playback if already playing
   static Future<void> pause() async {
     await checkIfBound();
     await _channel.invokeMethod("pause");
   }
 
-  /// Will resume playback (only if already loaded earlier and paused afterwards). If already playing will do nothing
+  /// Toggles audio depending on current playback state.
   static Future<void> resume() async {
     await checkIfBound();
     await _channel.invokeMethod("resume");
   }
 
+  /// Unbinds audio from services. Calling this will require to restart services
   static Future<void> unbind() async {
     await checkIfBound();
     await _channel.invokeMethod("unbind");
   }
 
+  /// Checks and binds audio services.
   static Future<void> checkIfBound() async {
     await _channel.invokeMethod("checkIfBound");
   }
